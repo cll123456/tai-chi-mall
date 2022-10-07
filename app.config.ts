ed79df4bb5d46900804b84bd079e8e0516b10180
Cll12345 <2642137848@qq.com>
2022-10-07 22:41:01 +0800

    init: 🎉 项目初始化

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  title: 'Hello Nuxt--twinkle',
+  theme: {
+    dark: true,
+    colors: {
+      primary: '#ff0000'
+    }
+  }
+})
---
+++ pages/home.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>home</div>
+  <hello-world />
+</template>
